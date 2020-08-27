322c42ee820ce5999c280aa2d3bd2cc1f71f0b91
Flinn <358530466@qq.com>
2020-08-27 09:56:25 +0800

    支持size

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,17 +1,32 @@
 <template>
-  <button class="uiu-button" :class="{[`uiu-theme-${theme}`]: theme}">
+  <button class="uiu-button" :class="classes">
     <slot />
   </button>
 </template>
 
 <script lang="ts">
+import {computed} from 'vue'
 
 export default {
   props: {
     theme: {
       type: String,
       default: 'button'
+    },
+    size: {
+      type: String,
+      default: 'normal'
     }
+  },
+  setup(props) {
+    const {theme, size} = props
+    const classes = computed(() => {
+      return {
+        [`uiu-theme-${theme}`]: theme,
+        [`uiu-size-${size}`]: size
+      }
+    })
+    return {classes}
   }
 }
 </script>
@@ -64,6 +79,18 @@ $radius: 4px;
     color: inherit;
     &:hover,&:focus{
       background: darken(white, 5%);;
+    }
+  }
+  &.uiu-theme-button {
+    &.uiu-size-big{
+      font-size: 24px;
+      height: 48px;
+      padding: 0 16px
+    }
+    &.uiu-size-small{
+      font-size: 12px;
+      height: 20px;
+      padding: 0 4px;
     }
   }
 }
