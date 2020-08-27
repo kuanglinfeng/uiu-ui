5c01e664ea724b5329ee349df8365866ff618322
Flinn <358530466@qq.com>
2020-08-27 10:18:51 +0800

    加载中css

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,11 +1,12 @@
 <template>
-  <button class="uiu-button" :class="classes">
+  <button class="uiu-button" :class="classes" :disabled="disabled">
+    <span v-if="loading" class="uiu-loadingIndicator"></span>
     <slot />
   </button>
 </template>
 
 <script lang="ts">
-import {computed} from 'vue'
+import { computed } from 'vue'
 
 export default {
   props: {
@@ -16,17 +17,30 @@ export default {
     size: {
       type: String,
       default: 'normal'
+    },
+    level: {
+      type: String,
+      default: 'normal'
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    loading: {
+      type: Boolean,
+      default: false
     }
   },
   setup(props) {
-    const {theme, size} = props
+    const { theme, size, level } = props
     const classes = computed(() => {
       return {
-        [`uiu-theme-${theme}`]: theme,
-        [`uiu-size-${size}`]: size
+        [`uiu-theme-${ theme }`]: theme,
+        [`uiu-size-${ size }`]: size,
+        [`uiu-level-${ level }`]: level
       }
     })
-    return {classes}
+    return { classes }
   }
 }
 </script>
@@ -37,6 +51,8 @@ $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
+$red: red;
+$grey: grey;
 .uiu-button {
   box-sizing: border-box;
   height: $h;
@@ -51,6 +67,7 @@ $radius: 4px;
   border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
+  transition: background 250ms;
   & + & {
     margin-left: 8px;
   }
@@ -65,31 +82,110 @@ $radius: 4px;
   &::-moz-focus-inner {
     border: 0;
   }
-  &.uiu-theme-link{
+  &.uiu-theme-link {
     border-color: transparent;
     box-shadow: none;
     color: $blue;
-    &:hover,&:focus{
+    &:hover, &:focus {
       color: lighten($blue, 10%);
     }
   }
-  &.uiu-theme-text{
+  &.uiu-theme-text {
     border-color: transparent;
     box-shadow: none;
     color: inherit;
-    &:hover,&:focus{
+    &:hover, &:focus {
       background: darken(white, 5%);;
     }
   }
-  &.uiu-size-big{
+  &.uiu-size-big {
     font-size: 24px;
     height: 48px;
     padding: 0 16px
   }
-  &.uiu-size-small{
+  &.uiu-size-small {
     font-size: 12px;
     height: 20px;
     padding: 0 4px;
+  }
+  &.uiu-theme-button {
+    &.uiu-level-main {
+      background: $blue;
+      color: white;
+      border-color: $blue;
+      &:hover,
+      &:focus {
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
+    }
+    &.uiu-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
+      &:hover,
+      &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
+    }
+  }
+  &.uiu-theme-link {
+    &.uiu-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+  &.uiu-theme-text {
+    &.uiu-level-main {
+      color: $blue;
+      &:hover,
+      &:focus {
+        color: darken($blue, 10%);
+      }
+    }
+    &.uiu-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+  &.uiu-theme-button {
+    &[disabled] {
+      cursor: not-allowed;
+      color: $grey;
+      &:hover {
+        border-color: $grey;
+      }
+    }
+  }
+  &.uiu-theme-link, &.uiu-theme-text {
+    &[disabled] {
+      cursor: not-allowed;
+      color: $grey;
+    }
+  }
+  > .uiu-loadingIndicator {
+    width: 14px;
+    height: 14px;
+    display: inline-block;
+    margin-right: 4px;
+    border-radius: 8px;
+    border-color: $blue $blue $blue transparent;
+    border-style: solid;
+    border-width: 2px;
+    animation: uiu-spin 1s infinite linear;
+  }
+}
+
+@keyframes uiu-spin {
+  0% {transform: rotate(0deg)}
+  100% {transform: rotate(360deg)
   }
 }
 </style>
