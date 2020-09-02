afea2515ff7b665fa5da1245f2cd036d1bfe1a35
Flinn <358530466@qq.com>
2020-09-02 21:28:10 +0800

    使Dialog可以切换了

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -3,13 +3,23 @@
     dialog 示例
   </div>
   <h1>示例1</h1>
-  <Dialog></Dialog>
+  <button @click="toggle">toggle</button>
+  <Dialog :visible="x"></Dialog>
 </template>
 
 <script lang="ts">
 import Dialog from '../lib/Dialog.vue'
+import Button from '../lib/Button.vue'
+import { ref } from 'vue'
 
 export default {
-  components: {Dialog}
+  components: { Dialog, Button },
+  setup() {
+    const x = ref(false)
+    const toggle = () => {
+      x.value = !x.value
+    }
+    return { x, toggle }
+  }
 }
 </script>
